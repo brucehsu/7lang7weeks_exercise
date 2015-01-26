edafe400af74baf065492e3acc8ba5ea57f99498
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2015-01-26 16:44:58 +0800

    [Prolog] Day 1 Written by an author

---
+++ prolog/day1/1_authors.pl
@@ -0,0 +1,15 @@
+written_by(american_gods, neil_gaiman).
+written_by(anansi_boys, neil_gaiman).
+written_by(coraline, neil_gaiman).
+written_by(foundation, isaac_asimov).
+written_by(i_robot, isaac_asimov).
+
+% ?- ['authors']
+% ?- written_by(Book, neil_gaiman).
+% Book = american_gods ? a
+%
+% Book = anansi_boys
+%
+% Book = coraline
+%
+% yes
