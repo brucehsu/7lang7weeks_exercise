0ee7c5bbf2ba21177971c626313ca87dbcc090c6
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2015-01-29 12:15:49 +0800

    [Prolog] Day 2 Exercise 1 Reverse list using accumulator

--- prolog/day2/1_list_reverse.pl
+++ prolog/day2/1_list_reverse.pl
@@ -1,3 +1,5 @@
 reverse_list([Head|[]], [Head]).
 reverse_list([Head|[Head2|[]]], [Head2, Head]).
 reverse_list([Head|Tail], Reversed) :- reverse_list(Tail, ReversedTail), append(ReversedTail, [Head], Reversed).
+reverse_accu([], List, List).
+reverse_accu([Head|Tail], List, Res) :- reverse_accu(Tail, [Head|List], Res).
